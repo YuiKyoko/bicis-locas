bd47699594450c86294bd4d0fe894eb18a2d2b06
YuiKyoko <kimberlyesquivelgala@gmail.com>
2016-09-15 13:59:02 -0500

    corregido

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function validateForm(){
+		//nombre
     var nom = document.getElementById("name").value;
     if( nom == null || nom.length == 0 || nom == ""){
       alert("Escribe un nombre.");
@@ -7,29 +8,31 @@ function validateForm(){
     }else if ( /^[a-z]/.test(nom.charAt(0)) ){
         alert ("escriba el primer caracter en mayuscula")
     }
+    //apellido
     var apellido = document.getElementById("lastname").value;
     if( apellido == null || apellido.length == 0 || apellido == ""){
-      alert("Escribe tu apellido.");
+      alert("Escribe tu aºpellido.");
     }else if ( /[0-9]/.test(apellido)){
         alert("No ingrese números.")
     }else if ( /^[a-z]/.test(apellido.charAt(0)) ){
         alert ("Escriba el primer caracter en mayúscula.")
     }
+    //correo
     var emailRegex=/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
     cor = document.getElementById("input-email").value;
     if(!emailRegex.test(cor)) {
         alert("Ingrese un correo válido.");
     };
-    
+    //contraseña
     var contr = document.getElementById("input-password").value;
     if (contr.length <= 6 || contr.length == 0 || contr == "123456" || contr == "98754" || contr == "password" ) {
-        alert("Contraseña no válida.");
-        return false; 
+        alert("Contraseña no válida."); 
     }
-    
-    var opcion = document.getElementById("checkbox").selectedIndex;
-    if( opcion == null || opcion == "" ) {
-    alert("Por favor, seleccione una opción en checkbox.");
-    }
-    return true;    
+    //select
+    var opcion = document.querySelector("select").selectedIndex;
+		if( opcion == null || opcion == "" ) {
+		alert("Por favor, seleccione una opción en checkbox.");
+	}
+
+	return true;      
 }
